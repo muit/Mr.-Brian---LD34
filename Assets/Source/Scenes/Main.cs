75d18b00694abf3f2878d2cb52eae125723454e7
muit <miguel_3c@hotmail.com>
2015-12-13 22:27:56 +0100

    Tweaks, Fixes & Surprises

--- Assets/Source/Scenes/Main.cs
+++ Assets/Source/Scenes/Main.cs
@@ -18,10 +18,11 @@ public class Main : SceneScript
         BUTTONS_SETUP_DONE,
         GREETINGS_NO_ANSWER,
         GREETINGS_ANSWER,
-        EXAMPLE,
-        EXAMPLE_READY,
-        EXAMPLE_INSIST,
-        EXAMPLE_DECIDE
+        CASE,
+        CASE_READY,
+        CASE_INSIST,
+        CASE_DECIDE,
+        CASE_2
     }
 
     protected EventsMap events;
@@ -39,6 +40,7 @@ public class Main : SceneScript
     public Button greenButton;
     public Button blueButton;
     public SelectionText buttonTexts;
+    public GameObject[] spawnObjects;
 
     [Space()]
     public Canvas blueHelp;
@@ -55,6 +57,7 @@ public class Main : SceneScript
     {
         events.RegistryEvent((int)Events.PRESENTATION, 2000);
         AudioListener.pause = false;
+        Time.timeScale = 1;
         blueHelp.enabled = false;
         greenHelp.enabled = false;
     }
@@ -120,30 +123,37 @@ public class Main : SceneScript
                 blueHelp.enabled = false;
                 greenHelp.enabled = false;
                 console.Write("You are not very talkative today, Brian", 75);
-                events.RegistryEvent((int)Events.EXAMPLE, 5000);
+                events.RegistryEvent((int)Events.CASE, 5000);
                 break;
 
-            case Events.EXAMPLE:
+            case Events.CASE:
                 console.Write("Anyway, lets start.", 150);
                 console.Write("");
                 console.Write("Judge Mr. Brian (Nº241)");
                 console.Write("");
                 console.Write("Case Nº 72");
-                console.Write("Mr.Burke was an architect that built a cabin", 50);
-                console.Write("for Mr.Bender and he didn't take care of it.", 50);
-                console.Write("The cabin collapsed after 2 years.");
+                console.Write("Mr.Burke was an architect that built a cabin for Mr.Bender and he didn't take care of it.", 50);
+                console.Write("The cabin collapsed after 2 years and Bender was injured.");
                 console.Write("Whose fault it is?", 100);
-                events.RegistryEvent((int)Events.EXAMPLE_READY, 24000);
+                events.RegistryEvent((int)Events.CASE_READY, 20000);
                 break;
 
-            case Events.EXAMPLE_READY:
-                UnlockButtons(Events.EXAMPLE_DECIDE, "Mr. Burke", "Mr. Bender");
-                events.RegistryEvent((int)Events.EXAMPLE_INSIST, 20000);
+            case Events.CASE_READY:
+                UnlockButtons(Events.CASE_DECIDE, "Mr. Burke", "Mr. Bender");
+                events.RegistryEvent((int)Events.CASE_INSIST, 20000);
                 break;
 
-            case Events.EXAMPLE_INSIST:
+            case Events.CASE_INSIST:
                 console.Write("We have other cases to judge, Brian.", 75);
                 console.Write("Keep going my friend.", 100);
+                break;
+
+            case Events.CASE_2:
+                console.Write("");
+                console.Write("Okay, one less. Next case:", 150);
+                console.Write("");
+                console.Write("Case Nº 56");
+
                 break;
 
             default:
@@ -165,30 +175,31 @@ public class Main : SceneScript
                 blueHelp.enabled = false;
                 greenHelp.enabled = false;
                 console.Write("Why are you using the buttons to answer me?", 100);
-                events.RegistryEvent((int)Events.EXAMPLE, 5000);
+                events.RegistryEvent((int)Events.CASE, 5000);
                 break;
 
-            case Events.EXAMPLE_DECIDE:
-                events.CancelEvent((int)Events.EXAMPLE_INSIST);
+            case Events.CASE_DECIDE:
+                events.CancelEvent((int)Events.CASE_INSIST);
                 LockButtons();
                 console.Write("");
                 if (type == ButtonType.GREEN)
                 {
-                    console.Write("Mr. Bender", 100);
-                    console.Write("He was also my choose Mr.Brian! ", 100);
-                    console.Write("But Mr.Bender died.", 150);
-                    console.Write("He didnt like the verdict and wanned revenge.", 100);
-                    console.Write("Ok, next case.", 150);
+                    console.Write("Nice choose.", 100);
+                    console.Write("Maybe he could have done a better job with more budget.", 100);
+                    console.Write("Mr. Bender was always very greedy.", 150);
+                    console.Write("He always wanted to have more money...", 100);
+                    events.RegistryEvent((int)Events.CASE_2, 10000);
                 }
                 else
                 {
                     console.Write("So your choose is Mr. Bender", 100);
                     console.Write("He was also my choose Mr.Brian! ", 100);
-                    console.Write("But Mr.Bender died.", 150);
-                    console.Write("He didnt like the verdict and wanned revenge.", 100);
-                    console.Write("Ok, next case:", 150);
+                    console.Write("But Mr.Bender died. It's true that the cabin were cheap but...", 150);
+                    console.Write("He only wanted the best for his family and worked so hard for it.", 100);
+                    events.RegistryEvent((int)Events.CASE_2, 12000);
                 }
-                events.RegistryEvent((int)Events.EXAMPLE, 5000);
+
+                SpawnRandom();
                 break;
         }
 
@@ -208,6 +219,17 @@ public class Main : SceneScript
         greenButton.UnLock(eventId);
         buttonTexts.StartEvent();
         buttonTexts.SetTexts(greenValue, blueValue);
+    }
+
+    void SpawnRandom() {
+        for (int i = 0; i < spawnObjects.Length; i++) {
+            GameObject go = spawnObjects[Random.Range(0, spawnObjects.Length)];
+            if (go && !go.activeInHierarchy)
+            {
+                go.SetActive(true);
+                return;
+            }
+        }
     }
 
 }
